5ef3f9d3ac10b0f1129b99a09c1868a3091fca81
AXON <axonasif@gmail.com>
2022-11-15 13:05:34 +0000

    1st Component Progressbar finished

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,58 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--border-radius": '4px',
+    "--height": '8px'
+  },
+  medium: {
+    "--border-radius": '4px',
+    "--height": '12px'
+  },
+  large: {
+    "--border-radius": '4px',
+    "--height": '24px',
+    "--padding": '4px'
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const STYLES = SIZES[size];
+
+  return (
+    <>
+      <label>
+        <VisuallyHidden>Loading Progress</VisuallyHidden>
+
+        <ProgressBarEl determinate role="progressbar" style={STYLES} max='100' value={value}>
+          {value}
+        </ProgressBarEl>
+      </label>
+    </>
+  );
 };
+
+
+
+const ProgressBarEl = styled.progress`
+  appearance: none;
+  border: none;
+  overflow: hidden;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35}; 
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  max-width: 370px;
+  height: var(--height);
+  padding: var(--padding);
+  &::-webkit-progress-value{
+    background-color: ${COLORS.primary};
+    border-radius: ${p => p.value > 99 && '4px'};
+  }
+  &::-webkit-progress-bar{
+    background-color: ${COLORS.transparentGray15};
+  }
+
+`
 
 export default ProgressBar;
