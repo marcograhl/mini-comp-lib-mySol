91a9c45cd4bbb4c3da4b33cb1610b1f4e0d540cc
AXON <axonasif@gmail.com>
2022-11-17 12:18:40 +0000

    added input Component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -3,17 +3,62 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 
-import Icon from '../Icon';
+import Icon from '../IconEm';
 import VisuallyHidden from '../VisuallyHidden';
+
+const SIZES = {
+  small:{
+    '--font-size': 14/16 + 'rem',
+  },
+  large:{
+    '--font-size': 18/16 + 'rem'
+  }
+}
 
 const IconInput = ({
   label,
   icon,
-  width = 250,
   size,
+  width = 250,
   placeholder,
 }) => {
-  return 'TODO';
+  const STYLES = SIZES[size]
+
+  return (
+    <WrapperLabel style={STYLES} width={width}>
+      <Icon id={icon} />
+      <Input type={label} placeholder={placeholder}/>
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </WrapperLabel>
+  );
 };
+
+const WrapperLabel = styled.label`
+  font-size: var(--font-size);
+  display: flex;
+  color: ${COLORS.gray700};
+  width: ${p=>p.width + 'px'};
+  align-items: center;
+  gap: 1ex;
+  border-bottom: 1px solid ${COLORS.black};
+  &:focus-within{
+    outline: 1px auto -webkit-focus-ring-color;
+    outline-offset: 3px;}
+`
+
+const Input = styled.input`
+  appearance: none;
+  border: none;
+  background: transparent;
+  font-size: inherit;
+  width: 100%;
+  &::placeholder{
+    color: ${COLORS.gray500}
+  }
+  &:focus{
+    outline: none;
+  }
+`
+
 
 export default IconInput;
